d51fae9dc2daedb229f8fdbbc21eaa843360fdff
dilippoudel <dilippoudel578@gmail.com>
2024-11-15 08:27:09 +0200

    Registered custom user auth in project

--- foodonline_main/settings.py
+++ foodonline_main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "accounts"
 ]
 
 MIDDLEWARE = [
@@ -82,7 +83,7 @@ DATABASES = {
         "HOST": config('DB_HOST')
     }
 }
-
+AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
