64367cb8d90712c58a2b80779cdac61da3baae92
dilippoudel <dilippoudel578@gmail.com>
2024-11-19 13:58:08 +0200

    Defined views to render user registration form

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render # noqa
+from django.http import HttpResponse
+from django.shortcuts import render
 
-# Create your views here.
+def registerUser(request):
+    return render(request, 'accounts/registerUser.html')
