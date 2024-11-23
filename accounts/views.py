57ebba90694ce8583c524ba7c831fba4221016f6
dilippoudel <dilippoudel578@gmail.com>
2024-11-23 12:10:24 +0200

    Implemented create_user method to create users

--- accounts/views.py
+++ accounts/views.py
@@ -8,9 +8,22 @@ def registerUser(request):
         print(request.POST)
         form = UserForm(request.POST)
         if form.is_valid():
-            user = form.save(commit=False)
+            """Create the user using the form."""
+            # password = form.cleaned_data['password']
+            # user = form.save(commit=False)
+            # user.set_password(password)
+            # user.role = User.CUSTOMER
+            # user.save()
+            
+            """Create the user using create_user method."""
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
             user.role = User.CUSTOMER
-            form.save()
+            user.save()
             return redirect('registerUser')
     else:
         form = UserForm()
